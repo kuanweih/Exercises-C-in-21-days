08ed1e9c0eda5ea369225b3baad9149e36d175a6
kuanweih <kuanweih@andrew.cmu.edu>
2018-07-19 17:45:54 -0400

    bug for ; in the for loop

--- Ex-1-3-symbolic-constant.c
+++ Ex-1-3-symbolic-constant.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
+/* symbolic constants*/
+#define LOWER 0         // lower limit of temp
+#define UPPER 300;    // upper limit of temp
+#define STEP 20;      // step size
+
 /* Fahrenheit-Celsius table */
-
 int main() {
-    float fahr, cels;
-    int lower, upper, step;
+    int fahr;
+    float cels;
 
-    lower = 0;      // lower limit of temp
-    upper = 300;    // upper limit of temp
-    step = 20;      // step size
-
-    fahr = lower;
-    while (fahr <= upper){
+    for (fahr=LOWER; fahr<=UPPER; fahr=fahr+STEP){
         cels = (5.0/9.0) * (fahr - 32.0);
-        printf("%3.0f %6.1f\n", fahr, cels);
-        fahr = fahr + step;
+        printf("%3d %6.1f\n", fahr, cels);
     }
 
     return 0;
