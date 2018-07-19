c0c667ff07b26b04c4a9e5055331d4908030e9f4
Kw Huang <kuanweih@andrew.cmu.edu>
2018-07-19 16:44:14 -0400

    not done yet

--- Ex-1-2-temperature.c
+++ Ex-1-2-temperature.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+/* Fahrenheit-Celsius table */
+
 int main() {
+    float fahr, cels;
+    int lower, upper, step; 
+
+
+
     printf("Hello world!\n");
     printf("Test \c \n");
     return 0;
