344f221a177e9de2599e51c39ebc018891e53776
kuanweih <kuanweih@andrew.cmu.edu>
2018-07-19 17:29:10 -0400

    done

--- Ex-1-2-temperature.c
+++ Ex-1-2-temperature.c
@@ -4,11 +4,18 @@
 
 int main() {
     float fahr, cels;
-    int lower, upper, step; 
+    int lower, upper, step;
 
+    lower = 0;      // lower limit of temp
+    upper = 300;    // upper limit of temp
+    step = 20;      // step size
 
+    fahr = lower;
+    while (fahr <= upper){
+        cels = (5.0/9.0) * (fahr - 32.0);
+        printf("%3.0f %6.1f\n", fahr, cels);
+        fahr = fahr + step;
+    }
 
-    printf("Hello world!\n");
-    printf("Test \c \n");
     return 0;
 }
