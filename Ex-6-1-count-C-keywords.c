71e9e42807ae3373364caa6d360b5eb64c18338b
Kw Huang <kuanweih@andrew.cmu.edu>
2018-07-23 09:51:17 -0400

    not done yet..

---
+++ Ex-6-1-count-C-keywords.c
@@ -0,0 +1,28 @@
+#include  <stdio.h>
+#include  <ctype.h>
+#include  <string.h>
+
+#define   MAXWORD  100
+
+int  getword(char *, int);
+int  binsearch(char *, struct key *, int);
+
+// count C keywords
+int main()
+{
+    int n;
+    char word[MAXWORD];
+
+    while (getword(word, MAXWORD) != EOF) {
+        if (isalpha(word[0]))
+            if ((n = binsearch(word, keytab, NKEYS)) >= 0)
+                keytab[n].count++;
+    } // while
+
+    for (n = 0; n < NKEYS; n++) {
+        if (keytab[n].count > 0)
+            printf("%4d %s\n", keytab[n].count, keytab[n].word);
+    } // for
+  
+    return 0;
+} // main
